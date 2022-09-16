7bfbe6e1c8d24a4867852e7dbf4d22d7ca0e0b53
School <school@Sohails-MacBook-Pro-2.local>
2022-09-15 20:41:11 -0400

    Add date and for loop

--- week_1/index.js
+++ week_1/index.js
@@ -1,5 +1,4 @@
-// Import Lodash
-const _ = require('lodash');
+const _ = require("lodash")
 const Person = require("./person")
 
 const person1 = new Person("John Smith", 25)
@@ -12,21 +11,42 @@ let people = [
     new Person("Fred Smith", 20)
 ]
 
-// console.log("Hello World!")
-person1.greeting()
+// console.log(person1)
+// console.log(people)
+// person1.greeting()
+// people[2].greeting()
 
-// Get random person from array using lodash
+// For loop over people
+for (const person_obj of people){
+    person_obj.greeting()
+}
+
 random_person = _.sample(people)
-console.log(random_person)
-random_person.greeting() 
+// console.log(random_person)
+// random_person.greeting()
 
-// Use findIndex to get index of queried object/value
-person1_index = _.findIndex(people, person1);
-random_person_index = _.findIndex(people, random_person);
+
+person1_index = _.findIndex(people, person1)
+random_person_index = _.findIndex(people, random_person)
 
 // console.log(person1_index)
 // console.log(random_person_index)
+// people[random_person_index].greeting()
 
-// Get difference between people array and provided array
-difference = _.difference(people, [person1, random_person])
-console.log(difference)
+
+var sliced_array = _.take(people, 2)
+console.log(sliced_array)
+
+
+// different_people = _.difference(people, [person1, random_person])
+// console.log(different_people)
+
+// var concat_array = _.concat(people, [person1, random_person])
+// console.log(concat_array)
+
+var union_array = _.union(people, sliced_array)
+console.log(union_array)
+
+today = Date.now()
+christmas = new Date("2022/12/25")
+console.log((christmas.getTime() - today) / (1000 * 24 * 60 * 60))
